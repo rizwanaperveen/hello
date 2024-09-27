abddcbc0c2474257f053707e96c9b94cc6869524
Rizwana <159348120+rizwanaperveen@users.noreply.github.com>
2024-09-27 13:59:08 +0500

    Add files via upload
    Hello World

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+export default function Hello(){
+    return(
+        <div>
+            <h1>"HELLO WORLD"</h1>
+        </div>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Hello from "../../component/hello"
+export default function Home() {
+  return (
+    
+      <div className="bg-black text-orange-500 h-screen w-screen text-center font-serif text-7xl ">
+        <Hello/>
+       <img className="h-96 w-screen mt-5" src="https://th.bing.com/th/id/R.592aa6f6fe536efaae4a2fcd4a06de6e?rik=XxlI2e3OALt8ug&riu=http%3a%2f%2fworldtransformed.com%2fwp-content%2fuploads%2f2015%2f01%2fNextButton.jpg&ehk=NT0HhCNiR1B0KNvsmy4pi5ikVRLiQrx8dfTY9jUkLE0%3d&risl=&pid=ImgRaw&r=0" alt="pic" />
+      </div>
+  );
+}
